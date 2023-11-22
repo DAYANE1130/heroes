14d1e8446e4e1f884a00511d45a6b9bcc2b58881
Dayane <dayanebmartins@gmail.com>
2023-11-22 18:04:41 -0300

    reorganizando arquitetura criaçao da pasta pages e index que ira abrigar a pagina principal

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Esta é a página inicial</h1>
+      <p>Aqui você pode adicionar seu conteúdo específico para esta página.</p>
+    </div>
+  );
+}
