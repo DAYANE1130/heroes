71aa8e6bbfacd28ae6cd74ace6c6d27d7a11d360
Dayane <dayanebmartins@gmail.com>
2023-11-22 21:12:33 -0300

    cria paginas que serao as rotas da aplicaçao

---
+++ src/pages/combat.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>COMBAAAAAAAT</h1>
+      <p>Aqui você pode adicionar seu conteúdo específico para esta página.</p>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,29 @@
 
-export default function Home() {
+export async function getStaticProps() {
+  const result = await fetch("http://homologacao3.azapfy.com.br/api/ps/metahumans");
+  const heroes = await result.json();
+  return {
+    props: {
+      heroes,
+    },
+  };
+}
+
+export default function Home({ heroes }) {
   return (
     <div>
       <h1>Esta é a página inicial</h1>
       <p>Aqui você pode adicionar seu conteúdo específico para esta página.</p>
+      <section>
+        {heroes.map((hero) => (
+          <div key={hero.id}>
+            <h2>{hero.name}</h2>
+            <p>Gender: {hero.appearance.gender}</p>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
+
+
